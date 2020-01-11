67d64d0caff9d5ab8173a8c6cb158a50090abf01
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2020-01-11 10:14:58 +0200

    add files

---
+++ DeliveryVehicleList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amitm on 11/01/2020.
+//
+
+#include "DeliveryVehicleList.h"
---
+++ DeliveryVehicleList.h
@@ -0,0 +1,14 @@
+//
+// Created by amitm on 11/01/2020.
+//
+
+#ifndef HW5_DELIVERYVEHICLELIST_H
+#define HW5_DELIVERYVEHICLELIST_H
+
+
+class DeliveryVehicleList {
+
+};
+
+
+#endif //HW5_DELIVERYVEHICLELIST_H
