789ea412387d7e895674836553c842db04891b10
omercohen7640 <45284523+omercohen7640@users.noreply.github.com>
2020-01-11 13:23:09 +0200

    adding setters and getters

--- DeliveryVehicle.cpp
+++ DeliveryVehicle.cpp
@@ -3,19 +3,43 @@
 //
 #include "DeliveryVehicle.H"
 
-DeliveryVehicle(){}
-
-
-DeliveryVehicle::DeliveryVehicle() {}
+DeliveryVehicle::DeliveryVehicle() {
+    _next_vehicle = NULL;
+}
 
 DeliveryVehicle::DeliveryVehicle(const char *ID, Quality quality): _vehicle_quality(quality) {
     strcpy(_vehicle_id,ID);
+    _next_vehicle = NULL;
 }
 
 DeliveryVehicle::~DeliveryVehicle() {
     delete _vehicle_id;
-    dele
 }
+
+Quality DeliveryVehicle::getQuality() {
+    return _vehicle_quality;
+}
+
+char *DeliveryVehicle::getID() const {
+    return _vehicle_id;
+}
+
+DeliveryVehicle *DeliveryVehicle::getNext() const {
+    return _next_vehicle;
+}
+
+void DeliveryVehicle::setNext(DeliveryVehicle *next_vehicle) {
+    _next_vehicle = next_vehicle;
+}
+
+int DeliveryVehicle::performDeliveryDay(int *numberOfDeliveries) {
+
+}
+
+
+
+
+
 
 
 
