e80bcf82360a1426363d9d0794dd4d4d26e524ca
omercohen7640 <45284523+omercohen7640@users.noreply.github.com>
2020-01-11 13:00:51 +0200

    adding next vehicle

--- DeliveryVehicle.cpp
+++ DeliveryVehicle.cpp
@@ -1,4 +1,21 @@
 //
 // Created by amitm on 11/01/2020.
 //
+#include "DeliveryVehicle.H"
+
+DeliveryVehicle(){}
+
+
+DeliveryVehicle::DeliveryVehicle() {}
+
+DeliveryVehicle::DeliveryVehicle(const char *ID, Quality quality): _vehicle_quality(quality) {
+    strcpy(_vehicle_id,ID);
+}
+
+DeliveryVehicle::~DeliveryVehicle() {
+    delete _vehicle_id;
+    dele
+}
+
+
 
--- Parcel.cpp
+++ Parcel.cpp
@@ -2,3 +2,26 @@
 // Created by amitm on 11/01/2020.
 //
 
+#include "Parcel.H"
+
+Parcel::Parcel(const char *ID , int dest ): _dest(dest) {
+    strcpy(_id,ID);
+}
+
+Parcel::~Parcel() {
+    delete _id;
+}
+
+int Parcel::getDest(){
+    return _dest;
+}
+
+char* Parcel::getID() {
+    return _id;
+}
+
+
+
+
+
+
