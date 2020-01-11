9149aed55d802bf1c5c7f6fb3bb14199d80149c2
omercohen7640 <45284523+omercohen7640@users.noreply.github.com>
2020-01-11 19:22:18 +0200

    finish FastDeliveryVehicle.cpp + H

--- DeliveryVehicle.cpp
+++ DeliveryVehicle.cpp
@@ -4,17 +4,11 @@
 #include "DeliveryVehicle.H"
 
 DeliveryVehicle::DeliveryVehicle() {
-    for (int i = 0; i < PARCEL_NUM; ++i) {
-        parcel_arrey[i] = NULL;
-    }
     _next_vehicle = NULL;
 }
 
-DeliveryVehicle::DeliveryVehicle(const char *ID, Quality quality): _vehicle_quality(quality) {
+DeliveryVehicle::DeliveryVehicle(const char *ID, Quality quality): _vehicle_quality(quality), _current_station(0) {
     strcpy(_vehicle_id,ID);
-    for (int i = 0; i < PARCEL_NUM; ++i) {
-        parcel_arrey[i] = NULL;
-    }
     _next_vehicle = NULL;
 }
 
@@ -39,17 +33,48 @@ void DeliveryVehicle::setNext(DeliveryVehicle *next_vehicle) {
 }
 
 bool DeliveryVehicle::addParcel(Parcel *parcel) {
-    for (int i = 0; i < PARCEL_NUM; ++i) {
-        if (parcel_arrey[i]->getDest() == -1){
-            parcel_arrey[i] = parcel;
-            return true;
-        }
-    }
-    return false;
+     if (parcel_queue.size() == PARCEL_NUM) return false;
+     parcel_queue.push(parcel);
+     return true;
 }
 
 int DeliveryVehicle::performDeliveryDay(int *numberOfDeliveries) {
+    if (!parcel_queue.empty()){
+        cout << "No parcels to deliver for vehicle " << _vehicle_id << endl;
+        *numberOfDeliveries = 0;
+        return 0;
+    }
+    cout << "Starting deliveries for vehicle" << _vehicle_id << endl;
+    int station_counter = 0;
+    int distance;
+    int delivery_counter=0;
+    int total_revenue;
+    int next_station;
+    while (!parcel_queue.empty()){
+        next_station = parcel_queue.front()->getDest();
+        if (next_station >= _current_station)
+            distance = next_station-_current_station;
+        else
+            distance = 10-_current_station + next_station;
+        if (station_counter + distance > 12)
+        {
+            *numberOfDeliveries = delivery_counter;
+            total_revenue= 4*delivery_counter - _vehicle_quality -station_counter;
+            cout << "Total revenue is " << total_revenue;
+            return total_revenue;
+        }
 
+        station_counter+= distance;
+        delivery_counter++;
+
+        cout << "Delivering parcel " << parcel_queue.front()->getID() << " to position " << next_station << endl;
+        cout << "Fuel consumed: " << distance << " Revenue is: 4";
+        parcel_queue.pop();
+    }
+    *numberOfDeliveries = station_counter;
+    total_revenue= 4*delivery_counter - _vehicle_quality -station_counter;
+    cout << "Total revenue is " << total_revenue;
+    return total_revenue;
 }
 
 
--- FastDeliveryVehicle.cpp
+++ FastDeliveryVehicle.cpp
@@ -2,3 +2,53 @@
 // Created by amitm on 11/01/2020.
 //
 
+#include "FastDeliveryVehicle.H"
+
+FastDeliveryVehicle::FastDeliveryVehicle(const char *ID, Quality quality) {
+
+}
+
+FastDeliveryVehicle::~FastDeliveryVehicle() {
+
+}
+
+
+int FastDeliveryVehicle::performDeliveryDay(int *numberOfDeliveries) {
+    if (!parcel_queue.empty()){
+        cout << "No parcels to deliver for vehicle " << _vehicle_id << endl;
+        *numberOfDeliveries = 0;
+        return 0;
+    }
+    cout << "Starting deliveries for vehicle" << _vehicle_id << endl;
+    int station_counter = 0;
+    int distance;
+    int delivery_counter=0;
+    int total_revenue;
+    int next_station;
+    while (!parcel_queue.empty()){
+        next_station = parcel_queue.front()->getDest();
+        if (next_station >= _current_station)
+            distance = next_station-_current_station;
+        else
+            distance = 10-_current_station + next_station;
+        if (station_counter + distance > 12)
+        {
+            *numberOfDeliveries = delivery_counter;
+            total_revenue= station_counter - _vehicle_quality -station_counter;
+            cout << "Total revenue is " << total_revenue;
+            return total_revenue;
+        }
+
+        station_counter+= distance;
+        delivery_counter++;
+
+        cout << "Delivering parcel " << parcel_queue.front()->getID() << " to position " << next_station << endl;
+        cout << "Fuel consumed: " << distance << " Revenue is: 4";
+        parcel_queue.pop();
+    }
+    *numberOfDeliveries = station_counter;
+    total_revenue= 4*delivery_counter - _vehicle_quality -station_counter;
+    cout << "Total revenue is " << total_revenue;
+    return total_revenue;
+}
+
