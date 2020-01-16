e0d0b35d4d69ffd67c8de1ac34f0958c04252f8f
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2020-01-16 21:14:41 +0200

    company

--- FastDeliveryCompany.cpp
+++ FastDeliveryCompany.cpp
@@ -2,3 +2,17 @@
 // Created by amitm on 11/01/2020.
 //
 
+#include "FastDeliveryCompany.H"
+
+FastDeliveryCompany::FastDeliveryCompany(int money) : DeliveryCompany(money) {}
+
+//TODO: no implementation needed???
+FastDeliveryCompany::~FastDeliveryCompany() {}
+
+bool FastDeliveryCompany::addVehicle(DeliveryVehicle *vehicle) {
+
+    return DeliveryCompany::addVehicle(vehicle);
+}
+
+
+
--- ProfessionalDeliveryCompany.cpp
+++ ProfessionalDeliveryCompany.cpp
@@ -2,3 +2,15 @@
 // Created by amitm on 11/01/2020.
 //
 
+#include "ProfessionalDeliveryCompany.H"
+
+ProfessionalDeliveryCompany::ProfessionalDeliveryCompany(int money) : DeliveryCompany(money) {}
+
+ProfessionalDeliveryCompany::~ProfessionalDeliveryCompany() {}
+
+bool ProfessionalDeliveryCompany::addVehicle(DeliveryVehicle *vehicle) {
+    return DeliveryCompany::addVehicle(vehicle);
+}
+
+
+
