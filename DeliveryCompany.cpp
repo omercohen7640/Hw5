ea4dafd93b8222ccd8d4b5e35fd1cd3fdd7bb1c1
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2020-01-16 20:56:01 +0200

    company

--- DeliveryCompany.cpp
+++ DeliveryCompany.cpp
@@ -4,26 +4,30 @@
 
 #include "DeliveryCompany.H"
 
+
 DeliveryCompany::DeliveryCompany(int money) : balance_(money), revenue_(0), lastReceivedAParcel_(DV_List_.getTopVehicle()), numberOfDeliveries_(0){}
 
 DeliveryCompany::~DeliveryCompany() {}
 
 bool DeliveryCompany::addVehicle(DeliveryVehicle *vehicle) {
-    if (DV_List_.pushVehicle(*vehicle) == SUCCESS) return true;
+    if (DV_List_.pushVehicle(vehicle) == SUCCESS) return true;
     return false;
 }
 
 bool DeliveryCompany::receiveParcel(Parcel *parcel) {
-        list<DeliveryVehicle>::iterator current;
-        bool flag_parcelAdded;
-        if (DV_List_.isEmpty()) return false;
-
-        current = lastReceivedAParcel_;
-        if (current == DV_List_.getBottomVehicle())
+    if (DV_List_.isEmpty()) return false;
+    list<DeliveryVehicle*>::iterator current; //current is a pointer to pointer, because an iterator is a pointer to an element, and the elements in this list are pointers
+    bool flag_parcelAdded;
+    current = lastReceivedAParcel_;
+    if (current == DV_List_.getBottomVehicle())
         {
             current = DV_List_.getTopVehicle();
-            flag_parcelAdded = (*current).addParcel(parcel);
+            flag_parcelAdded = (**current).addParcel(parcel);
             if (flag_parcelAdded == true) return true;
+            else
+            {
+                if (parcel == NULL) return false;
+            }
         }
         ++current;
         while (current != lastReceivedAParcel_)
@@ -31,14 +35,22 @@ bool DeliveryCompany::receiveParcel(Parcel *parcel) {
             if (current != DV_List_.getBottomVehicle())
             {
                 ++current;
-                flag_parcelAdded = (*current).addParcel(parcel);
+                flag_parcelAdded = (**current).addParcel(parcel);
                 if (flag_parcelAdded == true) return true;
+                else
+                {
+                    if (parcel == NULL) return false;
+                }
             }
             else
             {
                 current = DV_List_.getTopVehicle();
-                flag_parcelAdded = (*current).addParcel(parcel);
+                flag_parcelAdded = (**current).addParcel(parcel);
                 if (flag_parcelAdded == true) return true;
+                else
+                {
+                    if (parcel == NULL) return false;
+                }
             }
         }
     return false;
@@ -47,17 +59,15 @@ bool DeliveryCompany::receiveParcel(Parcel *parcel) {
 bool DeliveryCompany::performDeliveryDay() {
     if (DV_List_.isEmpty() == true) return false;
     cout << "Starting days deliveries:" << endl;
-    list<DeliveryVehicle>::iterator it;
+    list<DeliveryVehicle*>::iterator it;
     revenue_ = 0;
     for (it = DV_List_.getTopVehicle(); it != DV_List_.getBottomVehicle() ; ++it)
     {
         int numberOfDeliveries = 0;
-        revenue_ += (*it).performDeliveryDay(&numberOfDeliveries);
+        revenue_ += (**it).performDeliveryDay(&numberOfDeliveries);
         numberOfDeliveries_ += numberOfDeliveries;
     }
     cout << "Total revenue for company is " << revenue_ << endl;
-    displayFunds();
-    displayNumberOfDeliveries();
     return true;
 }
 
@@ -68,4 +78,5 @@ void DeliveryCompany::displayFunds() {
 void DeliveryCompany::displayNumberOfDeliveries() {
     cout << "Till now company has delivered " << numberOfDeliveries_ << " parcels" << endl;
 }
+
 
