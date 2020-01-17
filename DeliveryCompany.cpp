3bb33f387d053e75a64dc1e7cc114dadb0c0cb3d
amitmiz-175 <57402092+amitmiz-175@users.noreply.github.com>
2020-01-17 11:09:13 +0200

    add parcel

--- DeliveryCompany.cpp
+++ DeliveryCompany.cpp
@@ -4,6 +4,12 @@
 
 #include "DeliveryCompany.H"
 
+void test (list<DeliveryVehicle*>::iterator lastReceivedAParcel_)
+{
+    list<DeliveryVehicle*>::iterator current;
+    current = lastReceivedAParcel_;
+    cout << "test1" << endl;
+}
 
 DeliveryCompany::DeliveryCompany(int money) : balance_(money), revenue_(0), lastReceivedAParcel_(DV_List_.getTopVehicle()), numberOfDeliveries_(0){}
 
@@ -27,43 +33,36 @@ bool DeliveryCompany::addVehicle(DeliveryVehicle *vehicle) {
 
 bool DeliveryCompany::receiveParcel(Parcel *parcel) {
     if (DV_List_.isEmpty()) return false;
-    list<DeliveryVehicle*>::iterator current; //current is a pointer to pointer, because an iterator is a pointer to an element, and the elements in this list are pointers
+    list<DeliveryVehicle *>::iterator current; //current is a pointer to pointer, because an iterator is a pointer to an element, and the elements in this list are pointers
     bool flag_parcelAdded;
     current = lastReceivedAParcel_;
-    if (current == DV_List_.getBottomVehicle())
-        {
-            current = DV_List_.getTopVehicle();
-            flag_parcelAdded = (**current).addParcel(parcel);
-            if (flag_parcelAdded == true) return true;
-            else
-            {
-                if (parcel == NULL) return false;
-            }
+    //if there's only one vehicle in the list
+    if (DV_List_.getTopVehicle() == DV_List_.getBottomVehicle()) {
+        //try to add parcel
+        flag_parcelAdded = (**current).addParcel(parcel);
+        //if parcel added successfully then change lastReceivedAParcel_ to this vehicle and end function
+        if (flag_parcelAdded == true) {
+            lastReceivedAParcel_ = current;
+            return true;
         }
-        ++current;
-        while (current != lastReceivedAParcel_)
-        {
-            if (current != DV_List_.getBottomVehicle())
-            {
-                ++current;
-                flag_parcelAdded = (**current).addParcel(parcel);
-                if (flag_parcelAdded == true) return true;
-                else
-                {
-                    if (parcel == NULL) return false;
-                }
-            }
-            else
-            {
-                current = DV_List_.getTopVehicle();
-                flag_parcelAdded = (**current).addParcel(parcel);
-                if (flag_parcelAdded == true) return true;
-                else
-                {
-                    if (parcel == NULL) return false;
-                }
-            }
+        //if parcel wasn't added to vehicle and parcel was deleted then end function
+        if (parcel == NULL) return false;
+    }
+    //if lastReceivedAParcel_ is the last vehicle in the list, we start check at the start of the list
+    lastReceivedAParcel_borderrs(current); //function does ++current
+    while (current != lastReceivedAParcel_) {
+        //try to add parcel
+        flag_parcelAdded = (**current).addParcel(parcel);
+        //if parcel added successfully then change lastReceivedAParcel_ to this vehicle and end function
+        if (flag_parcelAdded == true) {
+            lastReceivedAParcel_ = current;
+            return true;
         }
+        //if parcel wasn't added to vehicle and parcel was deleted then end function
+        if (parcel == NULL) return false;
+        //if parcel wasn't added to vehicle but still exists, try to find another vehicle
+        lastReceivedAParcel_borderrs(current); //function does ++current
+    }
     return false;
 }
 
@@ -89,6 +88,13 @@ void DeliveryCompany::displayFunds() {
 
 void DeliveryCompany::displayNumberOfDeliveries() {
     cout << "Till now company has delivered " << numberOfDeliveries_ << " parcels" << endl;
+}
+
+void DeliveryCompany::lastReceivedAParcel_borderrs(list<DeliveryVehicle *>::iterator &current) {
+    if (++current == DV_List_.getBottomVehicle())
+    {
+        current = DV_List_.getTopVehicle();
+    }
 }
 
 
